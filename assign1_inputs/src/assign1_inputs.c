f111618d4ccbe5def9e3b3c0094a8c90bbf5620f
Jordan Hoskins <jhoss1521@gmail.com>
2018-10-11 12:48:07 -0400

    merged Peter into Jordan

--- assign1_inputs/src/assign1_inputs.c
+++ assign1_inputs/src/assign1_inputs.c
@@ -10,7 +10,7 @@
 
 int main(void) {
 	printf("Enter the device");
-	printf("ls= left scan, rs= right scan, ws= weight scale, lo =left open, ro=right open, lc=left closed, rc = right closed , gru = guard right unlock, grl = guard right lock, gll=guard left lock,glu = guard left unlock");
+	printf("ls= left scann, rs= right scan, ws= weight scale, lo =left open, ro=right open, lc=left closed, rc = right closed , gru = guard right unlock, grl = guard right lock, gll=guard left lock,glu = guard left unlock");
 	scanf();
 	return EXIT_SUCCESS;
 }
