28c9c33de3fcb45140d6a44a6b32bbb71e2e81d6
Renzukon <pnguyenhoang@hotmail.com>
2018-10-11 12:20:56 -0400

    Added proj.h

--- assign1_display/src/assign1_display.c
+++ assign1_display/src/assign1_display.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <process.h>
+#include <sys/neutrino.h>
+#include <sys/netmgr.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void) {
 	printf("The display is running as process_id %d\n", getpid());
