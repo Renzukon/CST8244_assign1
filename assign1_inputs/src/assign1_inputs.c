e22d93a85305dc7ca8666ff058f2966ede5f2249
Jordan Hoskins <jhoss1521@gmail.com>
2018-10-11 13:30:28 -0400

    Added pete

--- assign1_inputs/src/assign1_inputs.c
+++ assign1_inputs/src/assign1_inputs.c
@@ -9,8 +9,40 @@
 #include "proj.h"
 
 int main(void) {
-	printf("Enter the event type");
-	printf("ls= left scan, rs= right scan, ws= weight scale, lo =left open, ro=right open, lc=left closed, rc = right closed , gru = guard right unlock, grl = guard right lock, gll=guard left lock,glu = guard left unlock");
-	scanf();
+	char choice[5];
+	currentState current;
+	State state;
+	int personid = 0;
+	int weight = 0;
+
+	printf("Enter the event type\n");
+	printf("ls = left scan, rs = right scan, ws = weight scale, lo = left open,\nro = right open, lc = left closed, rc = right closed , gru = guard right unlock,\ngrl = guard right lock, gll=guard left lock,glu = guard left unlock\n");
+	printf("or type exit to quit\n");
+	while(current.state != EXIT){
+		scanf("%s",&choice);
+		if(strcmp(choice,"ls") == 0){
+			state = LEFT_SCAN;
+			current.state = state;
+			printf("Please enter person id\n");
+			scanf("%d",personid);
+			current.personId = personid;
+		}else if(strcmp(choice,"rs") == 0){
+			state = RIGHT_SCAN;
+			current.state = state;
+			printf("Please enter person id\n");
+			scanf("%d",personid);
+			current.personId = personid;
+		}else if(strcmp(choice,"ws") == 0){
+			state = WEIGHT;
+			current.state = state;
+			printf("Please enter weight\n");
+			scanf("%d",weight);
+			current.personId = weight;
+		}else if(strcmp(choice,"exit") == 0){
+			state = EXIT;
+			current.state = state;
+		}
+	}
+	printf("Bye bye LOSER");
 	return EXIT_SUCCESS;
 }
