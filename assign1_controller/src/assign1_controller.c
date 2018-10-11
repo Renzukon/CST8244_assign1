7f738b95c8bd6a4f6df6dd7442ffd233184a8d4e
Renzukon <pnguyenhoang@hotmail.com>
2018-10-11 13:54:26 -0400

    jank

--- assign1_controller/src/assign1_controller.c
+++ assign1_controller/src/assign1_controller.c
@@ -5,12 +5,32 @@
 #include <sys/netmgr.h>
 #include <string.h>
 #include <ctype.h>
+#include <errno.h>
 
 #include "proj.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
+	int     rcvid;
+	int     chid;
+	currentState current;
+
 	printf("The controller is running as process_id %d\n", getpid());
 
-
+    chid = ChannelCreate (0);
+    if (chid == -1)
+    {
+    	perror("failed to create the channel.");
+    	exit (EXIT_FAILURE);
+    }
+    while (1) {
+    	rcvid = MsgReceive (chid, &current, sizeof (current), NULL);
+    	switch(current.state){
+    		case 0:
+    			current.outMessage = 0;
+    			break;
+    	}
+    }
+	MsgReply (rcvid, EOK, &current, sizeof(current));
+	printf("%d\n",current.state);
 	return EXIT_SUCCESS;
 }
