8116c108d79becbd9aca630aa4abd92cc1684b57
Renzukon <pnguyenhoang@hotmail.com>
2018-10-12 10:12:42 -0400

    updated Proj.h

--- assign1_display/src/proj.h
+++ assign1_display/src/proj.h
@@ -3,34 +3,97 @@
 #define PROJ_H_
 
 typedef enum {
-	LEFT_SCAN = 0,
-	RIGHT_SCAN = 1,
-	LEFT_OPEN = 2,
-	RIGHT_OPEN = 3,
-	LEFT_CLOSE = 4,
-	RIGHT_CLOSE = 5,
-	GUARD_LU = 6,
-	GUARD_LL = 7,
-	GUARD_RU = 8,
-	GUARD_RL = 9,
-	WEIGHT = 10,
-	EXIT = 11
+	INITIAL = 0,
+	LEFT_SCAN = 1,
+	RIGHT_SCAN = 2,
+	LEFT_OPEN = 3,
+	RIGHT_OPEN = 4,
+	LEFT_CLOSE = 5,
+	RIGHT_CLOSE = 6,
+	GUARD_LU = 7,
+	GUARD_LL = 8,
+	GUARD_RU = 9,
+	GUARD_RL = 10,
+	WEIGHT = 11,
+	EXIT = 12
 } State;
 
+#define NUM_INPUTS 13;
+typedef enum {
+	INITIAL = 0,
+	LEFT_SCAN = 1,
+	RIGHT_SCAN = 2,
+	LEFT_OPEN = 3,
+	RIGHT_OPEN = 4,
+	LEFT_CLOSE = 5,
+	RIGHT_CLOSE = 6,
+	GUARD_LU = 7,
+	GUARD_LL = 8,
+	GUARD_RU = 9,
+	GUARD_RL = 10,
+	WEIGHT = 11,
+	EXIT = 12
+} Input;
+
+const char *inMessage[NUM_INPUTSS] = {
+		"",
+		"ls",
+		"rs",
+		"lo",
+		"ro",
+		"lc",
+		"rc",
+		"glu",
+		"gll",
+		"gru",
+		"grl",
+		"ws",
+		"exit"
+};
+
+#define NUM_OUTPUTS 13	// where n equals the number of output messages from the FSM.
+typedef enum {
+	INITIAL = 0,
+	LEFT_SCAN = 1,
+	RIGHT_SCAN = 2,
+	LEFT_OPEN = 3,
+	RIGHT_OPEN = 4,
+	LEFT_CLOSE = 5,
+	RIGHT_CLOSE = 6,
+	GUARD_LU = 7,
+	GUARD_LL = 8,
+	GUARD_RU = 9,
+	GUARD_RL = 10,
+	WEIGHT = 11,
+	EXIT = 12
+} Output;
+
+const char *outMessage[NUM_OUTPUTS] = {
+	"",
+	"Left Scan",
+	"Right Scan",
+	"Left Open",
+	"Right Open",
+	"Left Close",
+	"Right Close",
+	"Guard Left Unlock",
+	"Guard Left Lock",
+	"Guard Right Unlock",
+	"Guard Right Lock",
+	"Weighing ",
+	"Exiting"
+};
+
 struct currentState {
-	int choice;
 	int state;
-	int next;
 	int direction;
 	int personId;
 	int weight;
 }typedef currentState;
 
 struct response {
-	char response[128];
-	int weight;
-	int personId;
-	int state;
+	currentState current;
+	int code;
 }typedef response;
 
 #endif
